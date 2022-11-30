37f15e8fa8d549ffe28c7b4e88481a179c8f4154
Sabine Eitel <Seit201@aol.com>
2022-11-30 09:19:04 +0100

    abstract

--- com/cc/java/Worker.java
+++ com/cc/java/Worker.java
@@ -6,4 +6,10 @@ public class Worker extends HoneyBee{
         return "I'm a worker bee, I work all day!";
     }
 
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return "I believe, I can fly at 6mph!";
+    }
+
 }
