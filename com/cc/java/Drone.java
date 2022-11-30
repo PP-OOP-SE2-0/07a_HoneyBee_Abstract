6aa65fb141c9482c2fd16de5548009662ddf036a
Sabine Eitel <Seit201@aol.com>
2022-11-30 09:18:53 +0100

    abstract

--- com/cc/java/Drone.java
+++ com/cc/java/Drone.java
@@ -5,6 +5,12 @@ public class Drone extends HoneyBee{
     public String doYourJob() {
         return "I'm a drone, I'm going to date our Queen!";
     }
+
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return "I believe, I can fly at 6mph!";
+    }
     
 
 }
